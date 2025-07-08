12b2af940b8fce2a3d5b820f66fe515c4625782c
NaimSdn <saadoune.naim.pro@gmail.com>
2025-07-09 00:06:52 +0200

    Update, adding way to get inside a house

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,8 +17,17 @@ func load_level(scene_path: String, spawn_point_name : String = "PlayerSpawnPoin
 	
 	level_root.add_child(new_level)
 	
-	var spawn_point : Node2D = new_level.get_node_or_null(spawn_point_name)
+	var spawn_point : Node2D = find_node_recursive(new_level, spawn_point_name)
 	if spawn_point:
 		player.global_position = spawn_point.global_position
 	else:
 		push_warning("Spawn point not found : %s" % spawn_point_name)
+		
+func find_node_recursive(root: Node, node_name: String) -> Node:
+	if root.name == node_name:
+		return root
+	for child in root.get_children():
+		var found : Node = find_node_recursive(child, node_name)
+		if found != null:
+			return found
+	return null
