c36329ff29c3379422ec248a37ae896bf455f639
NaimSdn <saadoune.naim.pro@gmail.com>
2025-07-09 00:10:54 +0200

    Call deffered to load level

--- scripts/town/door.gd
+++ scripts/town/door.gd
@@ -7,4 +7,4 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		var root : Node2D = get_tree().current_scene
 		if root.has_method("load_level"):
-			root.load_level(target_scene_path, target_spawn_point_name)
+			root.call_deferred("load_level", target_scene_path, target_spawn_point_name)
